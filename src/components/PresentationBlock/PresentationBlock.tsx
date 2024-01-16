ce968b10519e81712231f131dcb70dc4dfaa99cf
Vladimir-Murzin <grosswladimir@yandex.ru>
2024-01-16 18:29:55 +0300

    created PresentationBlock component

--- src/components/PresentationBlock/PresentationBlock.tsx
+++ src/components/PresentationBlock/PresentationBlock.tsx
@@ -1,9 +1,24 @@
-import React from 'react'
-import './PresentationBlock.scss'
+import React from 'react';
+import './PresentationBlock.scss';
+import Content from '../Content/Content.tsx';
+import {ReactComponent as LayerIcon} from '../../images/Layer.svg'
 
-const PresentationBlock = () => {
+const PresentationBlock: React.FC = () => {
     return (
-        <p>Hello</p>
+        <div className='presentationBlock'>
+            <Content>
+                <div className='presentationBlock__content'>
+                    <div>
+                        <LayerIcon/>
+                    </div>
+                    <div className='presentationBlock__productPresentation'>
+                        <h1 className='presentationBlock__title'>Реплики картин от <span className='presentationBlock__galleryName'>Ink. House</span></h1>
+                        <p className='presentationBlock__artDescription'>Высокое качество отрисовки на плотной бумаге или льняном холтсе. Редкие произведения, доступные цены</p>
+                        <button className='presentationBlock__button'>Продукция</button>
+                    </div>
+                </div>
+            </Content>
+        </div>
     );
 };
 
