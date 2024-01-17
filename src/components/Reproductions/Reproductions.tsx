026f0da67cc9e07c089d592903e37e41336d0f7d
Vladimir-Murzin <grosswladimir@yandex.ru>
2024-01-17 19:54:07 +0300

    added logic for buttons to the reproduction block

--- src/components/Reproductions/Reproductions.tsx
+++ src/components/Reproductions/Reproductions.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './Reproductions.scss';
 import { france } from '../../config/config.tsx';
 import { germany } from '../../config/config.tsx';
@@ -6,7 +6,10 @@ import { england } from '../../config/config.tsx';
 import Content from '../Content/Content.tsx';
 import Reproduction from '../Reproduction/Reproduction.tsx';
 
+
 const Reproductions = () => {
+    const [country, setCountry] = useState(france);
+
     return (
         <div className='reproductions'>
             <Content>
@@ -14,15 +17,30 @@ const Reproductions = () => {
                     <div className='reproductions__header'>
                         <h1 className='reproductions__title'>Репродукции</h1>
                             <div className='reproductions__titleButtons'>
-                                <button>Франция</button>
-                                <button>Германия</button>
-                                <button>Англия</button>
+                                <button 
+                                    className={`${country === france ? 'reproductions__activeButton' : ''}`} 
+                                    onClick={() => setCountry(france)}
+                                >
+                                    Франция
+                                </button>
+                                <button 
+                                    className={`${country === germany ? 'reproductions__activeButton' : ''}`}
+                                    onClick={() => setCountry(germany)}
+                                >
+                                    Германия
+                                </button>
+                                <button 
+                                    className={`${country === england ? 'reproductions__activeButton' : ''}`}
+                                    onClick={() => setCountry(england)}
+                                >
+                                    Англия
+                                </button>
                             </div>
                     </div>
 
                     <div className='reproductions__cards'>
                         {
-                            germany.map(el => {
+                            country.map(el => {
                                 return (
                                     <Reproduction key={el.name} reproduction={el}/>
                                 )
