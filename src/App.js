122077cc36553abf580d56295f42b201184321a8
Vladimir-Murzin <grosswladimir@yandex.ru>
2024-01-23 19:31:10 +0300

    created a HomePage, added routing

--- src/App.js
+++ src/App.js
@@ -1,21 +1,18 @@
 import './App.css';
 import Footer from './components/Footer/Footer.tsx';
 import Header from './components/Header/Header.tsx';
-import NewCollection from './components/NewCollection/NewCollection.tsx';
-import PresentationBlock from './components/PresentationBlock/PresentationBlock.tsx';
-import Reproductions from './components/Reproductions/Reproductions.tsx';
-import TeamDescription from './components/TeamDescription/TeamDescription.tsx';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import HomePage from './pages/HomePage/HomePage.tsx';
 
 function App() {
   return (
-    <>
+    <BrowserRouter>
       <Header/>
-      <PresentationBlock/>
-      <Reproductions/>
-      <NewCollection/>
-      <TeamDescription />
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+      </Routes>
       <Footer />
-    </>
+    </BrowserRouter>
   );
 }
 
---
+++ src/pages/HomePage/HomePage.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import PresentationBlock from '../../components/PresentationBlock/PresentationBlock.tsx';
+import Reproductions from '../../components/Reproductions/Reproductions.tsx';
+import NewCollection from '../../components/NewCollection/NewCollection.tsx';
+import TeamDescription from '../../components/TeamDescription/TeamDescription.tsx';
+
+const HomePage: React.FC = () => {
+    return (
+        <>
+            <PresentationBlock />
+            <Reproductions />
+            <NewCollection />
+            <TeamDescription />
+        </>
+    )
+}
+
+export default HomePage;
