e36b0f6ae14fd0ab2f2bc24796c18b2f515b2057
Vladimir-Murzin <grosswladimir@yandex.ru>
2024-02-28 21:01:13 +0300

    added fake backend for getting the paintings

---
+++ src/backend/index.ts
@@ -0,0 +1,57 @@
+import axios from 'axios';
+import MockAdapter from 'axios-mock-adapter';
+import { england, france, germany } from '../config/config.tsx';
+
+const axiosInstance = axios.create();
+const mock = new MockAdapter(axiosInstance, { delayResponse: 200 });
+
+// routes:
+// /paintings/:country - роут для получения картин определенной страны
+
+mock
+    .onGet(new RegExp('/paintings/[A-Za-z]+'))
+    .replyOnce(config => {
+        const country = config.url?.split('/')[2];
+
+        switch (country) {
+            case 'france':
+                return [200, {
+                    paintings: [...france]
+                }];
+            case 'germany':
+                return [200, {
+                    paintings: [...germany]
+                }];
+            case 'england':
+                return [200, {
+                    paintings: [...england]
+                }];
+            default:
+                return [404];
+        }
+    })
+    .onGet(new RegExp('/paintings/[A-Za-z]+'))
+    .networkErrorOnce()
+    .onGet(new RegExp('/paintings/[A-Za-z]+'))
+    .reply(config => {
+        const country = config.url?.split('/')[2];
+
+        switch (country) {
+            case 'france':
+                return [200, {
+                    paintings: [...france]
+                }];
+            case 'germany':
+                return [200, {
+                    paintings: [...germany]
+                }];
+            case 'england':
+                return [200, {
+                    paintings: [...england]
+                }];
+            default:
+                return [404];
+        }
+    });
+
+export default axiosInstance;
